3c214e7841de7f6952139f79b70d92038c75d306
sparklinglily <carlaejemeh@gmail.com>
2023-04-19 15:42:22 +0100

    created the profile settings page

---
+++ lib/features/menu/profile/medications.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Medications extends StatelessWidget {
+  const Medications({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
